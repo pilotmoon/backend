c50ef116c2e9391570a3734c399fa50d3d1be3be
Nick Moore <nick@pilotmoon.com>
2023-02-03 15:17:48 +0000

    Add typescript tooling

--- hellonode.js
+++ hellonode.js
@@ -1,23 +1,20 @@
 "use strict";
-
+Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
+const mongoose_1 = require("mongoose");
+// database test
+const testDb = async function () {
+    mongoose_1.default.set('strictQuery', false);
+    await mongoose_1.default.connect(process.env.DATABASE_URL ?? '', { dbName: 'testdb' });
+    mongoose_1.default.connection.useDb('testdb');
+    const Cat = mongoose_1.default.model('Cat', { name: String });
+    const kitty = new Cat({ name: 'Anton' });
+    kitty.save().then(() => console.log('meow'));
+};
+void testDb();
+// koa
 const app = new Koa();
-
-// Start function
-const test_db = async function() {
-  const mongoose = require("mongoose");
-  mongoose.set('strictQuery', false);
-  await mongoose.connect(process.env.DATABASE_URL, { dbName: "testdb" });  
-  mongoose.connection.useDb("testdb");
-  const Cat = mongoose.model("Cat", { name: String });
-  const kitty = new Cat({ name: "Anton" });
-  kitty.save().then(() => console.log("meow"));
-}
-
-test_db();
-
 app.use((ctx) => {
-  ctx.body = "Hello World, from koa.";
+    ctx.body = 'Hello World, from koa.';
 });
-
-app.listen(1234);
+app.listen(parseInt(process.env.APP_PORT ?? '8000'));
---
+++ hellonode.ts
@@ -0,0 +1,20 @@
+import Koa = require('koa')
+import mongoose from 'mongoose'
+
+// database test
+const testDb = async function (): Promise<void> {
+  mongoose.set('strictQuery', false)
+  await mongoose.connect(process.env.DATABASE_URL ?? '', { dbName: 'testdb' })
+  mongoose.connection.useDb('testdb')
+  const Cat = mongoose.model<any>('Cat', { name: String })
+  const kitty = new Cat({ name: 'Anton' })
+  kitty.save().then(() => console.log('meow'))
+}
+void testDb()
+
+// koa
+const app = new Koa()
+app.use((ctx) => {
+  ctx.body = 'Hello World, from koa.'
+})
+app.listen(parseInt(process.env.APP_PORT ?? '8000'))
