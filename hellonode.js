7bfa79af3544dbf48fc205df9da5911669684d46
Nick Moore <nick@pilotmoon.com>
2023-02-03 15:22:43 +0000

    Change cat name

--- hellonode.js
+++ hellonode.js
@@ -8,7 +8,7 @@ const testDb = async function () {
     await mongoose_1.default.connect(process.env.DATABASE_URL ?? '', { dbName: 'testdb' });
     mongoose_1.default.connection.useDb('testdb');
     const Cat = mongoose_1.default.model('Cat', { name: String });
-    const kitty = new Cat({ name: 'Anton' });
+    const kitty = new Cat({ name: 'Konstantin' });
     kitty.save().then(() => console.log('meow'));
 };
 void testDb();
--- hellonode.ts
+++ hellonode.ts
@@ -7,7 +7,7 @@ const testDb = async function (): Promise<void> {
   await mongoose.connect(process.env.DATABASE_URL ?? '', { dbName: 'testdb' })
   mongoose.connection.useDb('testdb')
   const Cat = mongoose.model<any>('Cat', { name: String })
-  const kitty = new Cat({ name: 'Anton' })
+  const kitty = new Cat({ name: 'Konstantin' })
   kitty.save().then(() => console.log('meow'))
 }
 void testDb()
