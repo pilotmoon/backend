88d6e320348a223b07508be77d06ba18b817cf89
Nick Moore <nick@pilotmoon.com>
2023-02-10 14:23:21 +0000

    Improved config loading

--- dist/src/config.js
+++ dist/src/config.js
@@ -1,8 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
-exports.config = {
-  DATABASE_URL: process.env.DATABASE_URL ?? "",
-  DATABASE_NAME: "testdb",
-  APP_PORT: parseInt(process.env.APP_PORT ?? "8000"),
-};
+// App configuration
+exports.config = {};
+// loader to read from process.env
+const envLoader = (key) => process.env[key];
+// construct a loader that always returns the same value
+const load = (value) => () => value;
+const noTransform = (string) => string;
+const decimalIntegerTransform = (string) => parseInt(string, 10);
+for (
+  const key of [
+    { key: "APP_PORT", transform: decimalIntegerTransform },
+    { key: "APP_URL" },
+    { key: "DATABASE_URL", secret: true },
+    { key: "DATABASE_NAME", loader: load("testdb") },
+  ]
+) {
+  loadEnv(key);
+}
+function loadEnv(
+  { key, loader = envLoader, transform = noTransform, secret = false },
+) {
+  const value = loader(key);
+  if (typeof value !== "string") {
+    throw new Error("Missing environment variable: " + key);
+  }
+  const trimmedValue = value.trim();
+  if (trimmedValue.length === 0) {
+    throw new Error("Empty environment variable: " + key);
+  }
+  const transformedValue = transform(trimmedValue);
+  console.log(
+    `Loaded config variable ${key.blue} with value ${
+      secret ? "<secret>".red : String(transformedValue).cyan
+    } as ${typeof transformedValue}`,
+  );
+  exports.config[key] = transformedValue;
+}
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,52 @@
-export const config = {
-  DATABASE_URL: process.env.DATABASE_URL ?? "",
-  DATABASE_NAME: "testdb",
-  APP_PORT: parseInt(process.env.APP_PORT ?? "8000"),
-};
+// App configuration
+export const config: { [key: string]: any } = {};
+
+interface Loader {
+  (key: string): string | undefined;
+}
+// loader to read from process.env
+const envLoader = (key: string) => process.env[key];
+// construct a loader that always returns the same value
+const load = (value: any) => () => value;
+
+interface Transformer {
+  (value: string): any;
+}
+const noTransform = (string: string) => string;
+const decimalIntegerTransform = (string: string) => parseInt(string, 10);
+
+for (
+  const key of [
+    { key: "APP_PORT", transform: decimalIntegerTransform },
+    { key: "APP_URL" },
+    { key: "DATABASE_URL", secret: true },
+    { key: "DATABASE_NAME", loader: load("testdb") },
+  ]
+) {
+  loadEnv(key);
+}
+
+function loadEnv(
+  { key, loader = envLoader, transform = noTransform, secret = false }: {
+    key: string;
+    loader?: Loader;
+    transform?: Transformer;
+    secret?: boolean;
+  },
+) {
+  const value = loader(key);
+  if (typeof value !== "string") {
+    throw new Error("Missing environment variable: " + key);
+  }
+  const trimmedValue = value.trim();
+  if (trimmedValue.length === 0) {
+    throw new Error("Empty environment variable: " + key);
+  }
+  const transformedValue = transform(trimmedValue);
+  console.log(
+    `Loaded config variable ${key.blue} with value ${
+      secret ? "<secret>".red : String(transformedValue).cyan
+    } as ${typeof transformedValue}`,
+  );
+  config[key] = transformedValue;
+}
