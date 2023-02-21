b4d16f4db9460baf37b4b38df62cfbcc9b9309dc
Nick Moore <nick@pilotmoon.com>
2023-02-21 10:28:41 +0000

    add more crypto tests

--- test/unit/testSecrets.ts
+++ test/unit/testSecrets.ts
@@ -7,3 +7,26 @@ test("encrypt and decrypt", (t) => {
   const decrypted = decryptString(encrypted, "test");
   t.is(decrypted, message);
 });
+
+test("bad key kind", (t) => {
+  const message = "hello world";
+  t.log(t.throws(() => encryptString(message, "foo" as any))?.message);
+});
+
+test("decrypt garbage", (t) => {
+  t.log(t.throws(() => decryptString("dsfkjsdhjfhdskjhfkj", "test"))?.message);
+});
+
+test("decrypt random iv + nothing", (t) => {
+  t.log(
+    t.throws(() => decryptString("12345678901234567890123456789012", "test"))
+      ?.message,
+  );
+});
+
+test("encrypt then modify iv", (t) => {
+  const message = "hello world";
+  const encrypted = encryptString(message, "test");
+  const modified = encrypted.slice(0, 32) + "12345678901234567890123456789012";
+  t.log(t.throws(() => decryptString(modified, "test"))?.message);
+});
