0596757fa272be640ed2d7f099b3a235da0edd08
Nick Moore <nick@pilotmoon.com>
2023-02-23 13:19:49 +0000

    add stub products router

---
+++ src/routers/products.ts
@@ -0,0 +1,12 @@
+import { assertScope } from "../authController";
+import { makeRouter } from "../koa";
+import { getDb } from "../database";
+import { config } from "../config";
+import { KeyKind } from "../identifiers";
+
+function getCollection(kind: KeyKind) {
+  const db = getDb(kind);
+  return db.collection("products");
+}
+
+export const router = makeRouter();
