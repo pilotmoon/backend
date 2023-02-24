cc82fdca2e9de05ad98ea6b0ffb415df2580f695
Nick Moore <nick@pilotmoon.com>
2023-02-24 09:07:09 +0000

    tidy enforceJson

--- src/middleware/enforceJson.ts
+++ src/middleware/enforceJson.ts
@@ -3,10 +3,10 @@ import { ApiError } from "../errors";
 
 // error if content-type is not application/json
 export async function enforceJson(ctx: Context, next: Next) {
-  const match = ctx.request.is("application/json");
-  const hasContent = typeof ctx.request.length === "number" &&
-    ctx.request.length > 0;
-  if (hasContent && match !== "application/json") {
+  const length = ctx.request.length;
+  const hasContent = typeof length === "number" && length > 0;
+  const isJson = ctx.request.is("application/json") === "application/json";
+  if (hasContent && !isJson) {
     throw new ApiError(415, "Content-Type must be application/json");
   }
   await next();
