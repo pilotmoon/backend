4a08cca57ec0a904bdd28d0f9ee14cf405a823e3
Nick Moore <nick@pilotmoon.com>
2023-03-09 20:52:33 +0000

    refactor errors files

--- src/api/middleware/handleError.ts
+++ src/api/middleware/handleError.ts
@@ -1,79 +1,10 @@
-import { ZodError } from "zod";
-import { fromZodError } from "zod-validation-error";
 import { Context, Next } from "koa";
-import { STATUS_CODES } from "node:http";
-import { ApiError } from "../../errors.js";
 import { log } from "../../logger.js";
-
-// type to represent information about an error
-interface ErrorInfo {
-  message: string;
-  type: string;
-  status: number;
-}
-
-// extract concrete info from an unknown error
-function getErrorInfo(error: unknown): ErrorInfo {
-  // get some kind of message
-  let message;
-  if (error instanceof ZodError) {
-    message = fromZodError(error).message;
-  } else if (error instanceof Error) {
-    message = error.message;
-  } else {
-    message = String(error);
-  }
-
-  // try to get type name from error
-  let type = "UnknownError";
-  if (
-    typeof error === "object" && error !== null &&
-    "name" in error && typeof error.name === "string"
-  ) {
-    type = error.name;
-  }
-
-  // adjust status code for known errors
-  let status = 500;
-  if (error instanceof ApiError) {
-    status = error.status;
-  } else if (error instanceof ZodError) {
-    status = 400;
-  }
-
-  return { message, type, status };
-}
-
-function httpStatusString(code: number, { showCode = true } = {}) {
-  const string = STATUS_CODES[code];
-  if (string) {
-    return showCode ? `${code} ${string}` : string;
-  } else {
-    return "???";
-  }
-}
-
-// pretty print status code for logging
-function prettyFormatStatus(status: number) {
-  let s = httpStatusString(status);
-  if (status >= 200 && status < 300) {
-    s = s.bgGreen;
-  } else if (status >= 400 && status < 500) {
-    s = s.bgYellow;
-  } else if (status >= 500 && status < 600) {
-    s = s.white.bgRed;
-  } else {
-    s = s.bgWhite;
-  }
-  return s;
-}
-
-// format the error info as it will be sent to the client
-function formatResponse(info: ErrorInfo) {
-  let result = httpStatusString(info.status, { showCode: false });
-  if (info.status < 500) result += ` (${info.message})`;
-  return result;
-}
+import {
+  formatResponse,
+  getErrorInfo,
+  prettyFormatStatus,
+} from "../../errors.js";
 
 export async function handleError(ctx: Context, next: Next) {
   let info;
@@ -83,6 +14,7 @@ export async function handleError(ctx: Context, next: Next) {
   } catch (error) {
     info = getErrorInfo(error);
     ctx.body = formatResponse(info);
+    ctx.set("X-Error-Message", info.message);
     ctx.status = info.status;
   } finally {
     log(
--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,7 @@
 import { ZodError } from "zod";
 import { MongoServerError } from "mongodb";
+import { fromZodError } from "zod-validation-error";
+import { STATUS_CODES } from "node:http";
 
 export class ApiError extends Error {
   status: number;
@@ -16,4 +18,77 @@ export function handleControllerError(error: unknown) {
   } else if (error instanceof ZodError) {
     throw new ApiError(500, "Invalid document: " + error.message);
   }
+}
+
+// type to represent information about an error
+interface ErrorInfo {
+  message: string;
+  type: string;
+  status: number;
+}
+
+// extract concrete info from an unknown error
+export function getErrorInfo(error: unknown): ErrorInfo {
+  // get some kind of message
+  let message;
+  if (error instanceof ZodError) {
+    message = fromZodError(error).message;
+  } else if (error instanceof Error) {
+    message = error.message;
+  } else {
+    message = String(error);
+  }
+
+  // try to get type name from error
+  let type = "UnknownError";
+  if (
+    typeof error === "object" && error !== null &&
+    "name" in error && typeof error.name === "string"
+  ) {
+    type = error.name;
+  }
+
+  // adjust status code for known errors
+  let status = 500;
+  if (error instanceof ApiError) {
+    status = error.status;
+  } else if (error instanceof ZodError) {
+    status = 400;
+  }
+
+  return { message, type, status };
+}
+
+// get the string for a status code
+function httpStatusString(code: number, { showCode = true } = {}) {
+  const string = STATUS_CODES[code];
+  if (string) {
+    return showCode ? `${code} ${string}` : string;
+  } else {
+    return "???";
+  }
+}
+
+// pretty print status code for logging
+export function prettyFormatStatus(status: number) {
+  let s = httpStatusString(status);
+  if (status >= 200 && status < 300) {
+    s = s.bgGreen;
+  } else if (status >= 400 && status < 500) {
+    s = s.bgYellow;
+  } else if (status >= 500 && status < 600) {
+    s = s.white.bgRed;
+  } else {
+    s = s.bgWhite;
+  }
+  return s;
+}
+
+// format the error info as it will be sent to the client in body
+export function formatResponse(info: ErrorInfo) {
+  let result = httpStatusString(info.status, { showCode: false });
+  if (info.status < 500) {
+    result += ` (${info.message})`;
+  }
+  return result;
 }
