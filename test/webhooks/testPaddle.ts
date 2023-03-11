876a206d73ccb8a24f26ae199c6f69b96ae74d27
Nick Moore <nick@pilotmoon.com>
2023-03-11 19:59:40 +0000

    change paddle test to com.example.product

--- test/webhooks/testPaddle.ts
+++ test/webhooks/testPaddle.ts
@@ -10,29 +10,29 @@ async function setup() {
 }
 
 const samplePaddle = {
-  name: "Test User",
-  quantity: "1",
+  email: "blackhole+640cdd5197baf@paddle.com",
+  event_time: "2023-03-11 19:58:09",
   marketing_consent: "0",
-  p_custom_data: "",
-  email: "blackhole+640a15b78705d@paddle.com",
-  p_paddle_fee: "1.35",
-  p_earnings: '{"9710":"15.6400"}',
-  p_quantity: "1",
-  p_used_price_override: "1",
-  p_sale_gross: "16.99",
-  p_currency: "USD",
-  p_coupon_savings: "0",
-  event_time: "2023-03-09 17:21:59",
-  p_signature:
-    "MH2fZXagiuiXWa4wPyf2r2kI/EFIj25OYge+b2OyxHbKKdf3fq30jLkvF7ndgrbGleJp3OF+4j/PEqt7TWDVx6nAT1vnH9RqGjGLDCLahtr1wPiFEun2skE02RqhUnsP6x/q8ZXx9weuoJxpiPhdpyVjh2S9IOyrxY2Q23kmpoPZWWGwLsetNQ9FVIY2tfYfvEEpCcpGVek4Mha1jPa9zmAOO+h9fcjg7t5zTWMXrg+YHzZFeGCn5CjOKkti8WEnlklr6pP1PFxCWIT0Hk2gAg+3zTY+HU1Cozks1bnz07TBXuh8fHGY1TnrpUSbq338IhEJREH5LDS/mt4KoWC1DgedG7bEjE2bAnFhDIWcg82rwHI7Ld+n5Oy5N/bdpJQgbWgSl2ckSJRxvbrpCXeBD472E5A4/0ArG2DmeU16wv8RJMrGO4a2StSyaiKv+uPiLS/wb1z+Xr7uJ3ajBxnqK329tbMxyGtpYprNHf5ZMV9JIw6LiCy25TuwOvTH0WKmIt/nnQElGxzY51uqEyiAwVo3RTB+3AygrZhxOSt5U2BTq8nAYK9fwfHjZrOEHJFP/u8lIjB6us26MpHisFh2Kh8zKL6SakefY09WxqH2eOOZgMPCzjhZCa68oW9pIGsJQQwUeuStry+lp1UTlwmsYhRdg7Cs47iWM13VKcc5omM=",
+  name: "Test User",
   p_country: "US",
-  p_price: "16.99",
-  p_order_id: "572168",
   p_coupon: "",
-  passthrough: "Example passthrough",
-  p_product_id: "41687",
+  p_coupon_savings: "0",
+  p_currency: "USD",
+  p_custom_data: "",
+  p_earnings: '{"9710":"14.0200"}',
+  p_order_id: "573239",
+  p_paddle_fee: "1.26",
+  p_price: "15.28",
+  p_product_id: "47023",
+  p_quantity: "1",
+  p_sale_gross: "15.28",
   p_tax_amount: "0",
-  product: "com.pilotmoon.popclip",
+  p_used_price_override: "1",
+  passthrough: "Example passthrough",
+  product: "com.example.product",
+  quantity: "1",
+  p_signature:
+    "UIWGXJsxIvfKlqRDZXTBzmtl0z+iVApe8VVBKaAvPwGlu/6aJMwo2IxWs1IYS3/KtLXJ5RwCWSKAaHGghzx4eOf1NcivTVC2hHCDKCCEaCyw6DEtVPbgd/8nAeQ8KoLm+FukJA5Utid340QCYurzGsFHoBOo2rxZVNTuWaFyGJ43R7zH0KaLHfacaMVbqg5Ev7Y1w/vR8geP/ovzWGvdFcFT7kiLfOlgVlSKAuzm0nNkV0vWsqpVFWC2f6XIL8x5DSmsUSSqxKbcxOJg4LGD5ZJ4MT/s6wXABYxI/TXSPeHhyNTMyRUQRPWm/XX9xDvMx1ADjFIel9/+MOoncxlG+VRTLSMNvpcSR6Z4xfBRt3rhTjCesmIoGRhlgOy53bW/BEP297Rfl75lmNBnvUOf6M881ro+dX6HzhfiWN6UFMd4T9QQkcrXjiq4MysxLmTulgY/qnuMle7I5fAHJnV/jjKueBLqSmk9JGA2N56YyHWmLWMiWi46aRTBb0+cQxzEVX9MrebiJX2iCuUTK4BiO2mdxnDxisnGE2eJJ6cpyC0sw4s3GgvQ+O2lpCOX5G8uxpQ1C9CALV6woCEmuHhACoo9xp2AeWJ4DmkVO2U3k8Cs47veWjfEY3auL3pBzt1pTmv1PQIfJQWdF7vVoEUDEbV930sBVfmax/OUQv5Im70=",
 };
 
 test.before(setup);
