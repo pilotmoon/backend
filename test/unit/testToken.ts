8e20debb4fc11dacc2c76a6d5beebc00e818bbdb
Nick Moore <nick@pilotmoon.com>
2023-04-20 11:29:41 +0100

    add test loops to other token test

--- test/unit/testToken.ts
+++ test/unit/testToken.ts
@@ -41,24 +41,27 @@ test("generate and decipher api key token", (t) => {
 });
 
 test("generate and decipher api key token with resource", (t) => {
-  const resource = "foo/bar";
-  const scopes = ["foo/bar:read", "foo/bar:write", "bingbong"];
-  const expiration = new Date();
-  const tokenString = token.generateEncryptedToken({
-    keyKind: "test",
-    scopes,
-    expires: expiration,
-    resource,
-  });
-  t.log(tokenString);
+  let count = 0;
+  while (count++ <= 1000) {
+    const resource = "foo/bar";
+    const scopes = ["foo/bar:read", "foo/bar:write", "bingbong"];
+    const expiration = new Date();
+    const tokenString = token.generateEncryptedToken({
+      keyKind: "test",
+      scopes,
+      expires: expiration,
+      resource,
+    });
+    //t.log(tokenString);
 
-  // decipher the token
-  const {
-    keyKind,
-    scopes: decipheredScopes,
-    expires: decipheredExpiration,
-  } = token.decipherToken(tokenString, resource);
-  t.is(keyKind, "test");
-  t.deepEqual(decipheredScopes, scopes);
-  t.deepEqual(decipheredExpiration, expiration);
+    // decipher the token
+    const {
+      keyKind,
+      scopes: decipheredScopes,
+      expires: decipheredExpiration,
+    } = token.decipherToken(tokenString, resource);
+    t.is(keyKind, "test");
+    t.deepEqual(decipheredScopes, scopes);
+    t.deepEqual(decipheredExpiration, expiration);
+  }
 });
