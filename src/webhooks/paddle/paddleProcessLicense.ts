3d0605ebbef6193f7365769033abc82e9b64a347
Nick Moore <nick@pilotmoon.com>
2023-04-27 20:15:24 +0100

    Add code to automatically void license on signal from paddle

--- src/webhooks/paddle/paddleProcessLicense.ts
+++ src/webhooks/paddle/paddleProcessLicense.ts
@@ -2,8 +2,10 @@ import { z } from "zod";
 import _ from "lodash";
 import { getRolo } from "../rolo.js";
 import { ZLicenseExternal } from "../../licenseFileObject.js";
+import { log } from "../../logger.js";
+import { ZLicenseKeyRecord } from "../../api/controllers/licenseKeysController.js";
 
-const ZPaddleArgs = z.object({
+const ZLicenseArgs = z.object({
   p_order_id: z.string(),
   p_quantity: z.string(),
   email: z.string(),
@@ -13,8 +15,7 @@ const ZPaddleArgs = z.object({
 
 export async function processLicense(args: unknown, mode: "test" | "live") {
   // create license
-  console.log("mode: ", mode);
-  const paddleArgs = ZPaddleArgs.passthrough().parse(args);
+  const paddleArgs = ZLicenseArgs.passthrough().parse(args);
   const api = getRolo(mode);
   const info = {
     email: paddleArgs.email,
@@ -27,4 +28,45 @@ export async function processLicense(args: unknown, mode: "test" | "live") {
   };
   const { data } = await api.post("/licenseKeys", info);
   return ZLicenseExternal.parse(data);
+}
+
+const ZRefundArgs = z.object({
+  order_id: z.string(),
+});
+const ZRefundResponse = z.object({
+  object: z.literal("list"),
+  items: z.array(z.object({
+    id: z.string(),
+    order: z.string().optional(),
+    origin: z.string().optional(),
+  })),
+});
+
+export async function processRefund(args: unknown, mode: "test" | "live") {
+  log("info", "Paddle refund", { args, mode });
+  const paddleArgs = ZRefundArgs.parse(args);
+  const api = getRolo(mode);
+  // find the order
+  const { data } = await api.get("/licenseKeys/byOrder/" + paddleArgs.order_id);
+  const response = ZRefundResponse.parse(data);
+  // find orders that match origin "Paddle"
+  log("response", response);
+  const paddleOrders = response.items.filter((item) =>
+    item.origin === "Paddle"
+  );
+  if (paddleOrders.length === 0) {
+    throw new Error("Paddle order not found");
+  }
+  if (paddleOrders.length > 1) {
+    throw new Error("Multiple Paddle orders found");
+  }
+  const order = paddleOrders[0];
+  // set the refunded flag on license key
+  const res = await api.patch(
+    "/licenseKeys/" + order.id,
+    { void: true },
+  );
+  if (res.status !== 204) {
+    throw new Error("Failed to update license key");
+  }
 }
--- src/webhooks/paddle/paddleRouter.ts
+++ src/webhooks/paddle/paddleRouter.ts
@@ -1,13 +1,15 @@
-import { processLicense } from "./paddleProcessLicense.js";
+import { processLicense, processRefund } from "./paddleProcessLicense.js";
 import { validatePaddleWebhook } from "./paddleValidateWebhook.js";
 import Router from "@koa/router";
 import { config } from "../config.js";
 import { ApiError } from "../../errors.js";
 import { paddleCredentials } from "../paddle.js";
+import { Context } from "koa";
+import { log } from "../../logger.js";
 
 export const router = new Router();
 
-router.post("/webhooks/paddle/generateLicense", async (ctx) => {
+function validate(ctx: Context) {
   const signers = [
     { pubkey: paddleCredentials.production.publicKey, mode: "live" as const },
     { pubkey: paddleCredentials.sandbox.publicKey, mode: "test" as const },
@@ -18,7 +20,19 @@ router.post("/webhooks/paddle/generateLicense", async (ctx) => {
   if (!signed) {
     throw new ApiError(400, "Invalid signature");
   }
+  log("Webhook signature verified, mode:", signed.mode);
+  return signed;
+}
+
+router.post("/webhooks/paddle/generateLicense", async (ctx) => {
+  const signed = validate(ctx);
   const { file } = await processLicense(ctx.request.body, signed.mode);
   const escapedName = file.name.replaceAll("_", "\\_");
   ctx.body = `[${escapedName}](${config.ROLO_URL_CANONICAL}${file.url})`;
+});
+
+router.post("/webhooks/paddle/alert/paymentRefunded", async (ctx) => {
+  const signed = validate(ctx);
+  processRefund(ctx.request.body, signed.mode);
+  ctx.status = 200;
 });
