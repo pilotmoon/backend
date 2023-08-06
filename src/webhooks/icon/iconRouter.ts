fc17c2b728417a150b565aa234832b6adc61fc98
Nick Moore <nick@pilotmoon.com>
2023-08-06 23:33:07 +0100

    add key generation for icons

--- src/webhooks/icon/iconRouter.ts
+++ src/webhooks/icon/iconRouter.ts
@@ -1,16 +1,17 @@
 import Router from "@koa/router";
-import { z } from "zod";
 import { getIcon } from "./iconController.js";
-import { canonicalizeColor } from "./key.js";
-export const router = new Router();
+import { canonicalize, generateKey, ZIconDescriptor } from "./key.js";
+import { LRUCache } from "lru-cache";
+import { Icon } from "./handler.js";
+import { log } from "console";
 
-const ZIconDescriptor = z.object({
-  specifier: z.string(),
-  color: z.string().optional(),
-});
+export const router = new Router();
+const cache = new LRUCache<string, Icon>({ max: 1000 });
 
 router.post(`/frontend/icon`, async (ctx) => {
-  const { specifier, color } = ZIconDescriptor.parse(ctx.request.body);
+  const { specifier, color } = canonicalize(
+    ZIconDescriptor.parse(ctx.request.body),
+  );
   const icon = getIcon(specifier, color);
   ctx.body = icon;
   ctx.status = 201;
@@ -18,12 +19,20 @@ router.post(`/frontend/icon`, async (ctx) => {
 });
 
 router.get(`/frontend/icon/:specifier`, async (ctx) => {
-  const { specifier } = ctx.params;
-  const { color } = ctx.query;
-  const icon = await getIcon(specifier, canonicalizeColor(color));
-  ctx.body = icon.data
+  const descriptor = canonicalize(ZIconDescriptor.parse({
+    specifier: ctx.params.specifier,
+    color: ctx.query.color,
+  }));
+  const key = generateKey(descriptor);
+  log("key: " + key);
+  const cachedIcon = cache.get(key);
+  if (cachedIcon) {
+    ctx.body = cachedIcon.data;
+    ctx.set("Content-Type", cachedIcon.contentType);
+    return;
+  }
+  const icon = await getIcon(descriptor.specifier, descriptor.color);
+  cache.set(key, icon);
+  ctx.body = icon.data;
   ctx.set("Content-Type", icon.contentType);
 });
-
-
-
--- src/webhooks/icon/key.ts
+++ src/webhooks/icon/key.ts
@@ -1,22 +1,45 @@
+import { z } from "zod";
+import { log } from "../../logger.js";
+import { sha256Base64Url } from "../../sha256.js";
 
-export function canonicalizeColor(color: any): string | undefined {
+export const ZIconDescriptor = z.object({
+  specifier: z.string(),
+  color: z.string().optional(),
+});
+
+export type IconDescriptor = z.infer<typeof ZIconDescriptor>;
+
+export function generateKey(descriptor: IconDescriptor): string {
+  const descriptorString = JSON.stringify(descriptor);
+  log("descriptorString: " + descriptorString);
+  return sha256Base64Url(descriptorString).substring(0, 24);
+}
+
+export function canonicalize(
+  descriptor: IconDescriptor,
+): IconDescriptor  {
+  return { ...descriptor, color: canonicalizeColor(descriptor.color) };
+}
+
+function canonicalizeColor(color: any): string | undefined {
   if (typeof color === "string") {
     color = color.trim();
     color = color.toLowerCase();
-    if (color === 'black') {
-      return '#000000';
+    if (color === "black") {
+      return "#000000";
     }
-    if (color === 'white') {
-      return '#ffffff';
+    if (color === "white") {
+      return "#ffffff";
     }
     if (color.startsWith("#")) {
       color = color.substring(1);
     }
     if (color.match(/^[0-9a-f]{6}$/i)) {
-      return '#' + color;
+      return "#" + color;
     }
     if (color.match(/^[0-9a-f]{3}$/i)) {
-      return "#" + color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
+      return "#" + color[0] + color[0] + color[1] + color[1] + color[2] +
+        color[2];
     }
   }
 }
