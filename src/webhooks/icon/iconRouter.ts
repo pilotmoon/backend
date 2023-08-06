eea971aebaec06029b937a27d081cec6a0d38365
Nick Moore <nick@pilotmoon.com>
2023-08-06 18:29:17 +0100

    add LRUCache for icon generation

--- src/webhooks/icon/iconRouter.ts
+++ src/webhooks/icon/iconRouter.ts
@@ -2,8 +2,13 @@ import Router from "@koa/router";
 import { ApiError } from "../../errors.js";
 
 import { getIcon as getIconPcx } from "./handlerPcx.js";
-import { IconFactory } from "./handler.js";
+import { Icon, IconFactory } from "./handler.js";
 import { recolor } from "./recolor.js";
+
+import { LRUCache } from 'lru-cache'
+
+const cache = new LRUCache<string, Icon>({max: 1000});
+
 export const router = new Router();
 
 const specifierRegex = /^([0-9a-z]+):(.+)$/;
@@ -13,7 +18,7 @@ const handlers: Record<string, IconFactory> = {
 };
 
 // proxying to the icons on the legacy server
-router.get(`/frontend/img/:specifier`, async (ctx) => {
+router.get(`/frontend/icon/:specifier`, async (ctx) => {
   const match = specifierRegex.exec(ctx.params.specifier);
   if (!match) {
     throw new ApiError(404, "Invalid specifier");
@@ -24,7 +29,7 @@ router.get(`/frontend/img/:specifier`, async (ctx) => {
     throw new ApiError(404, "Unknown prefix: " + prefix);
   }
 
-  let hexColor: string | undefined;
+  let hexColor: string = "000000";
   const hexColorRegex = /^([0-9a-f]{3}|[0-9a-f]{6})$/;
   if (typeof ctx.query.color === "string") {
     if (hexColorRegex.test(ctx.query.color)) {
@@ -33,8 +38,18 @@ router.get(`/frontend/img/:specifier`, async (ctx) => {
       throw new ApiError(400, "Invalid color specified");
     }
   }
+
+  const cacheKey = `${prefix}:${subspecifier}!${hexColor}`;
+  const cachedIcon = cache.get(cacheKey);
+  if (cachedIcon) {
+    console.log("Using cached icon for", cacheKey);
+    ctx.body = cachedIcon.data;
+    ctx.set("Content-Type", cachedIcon.contentType);
+    return;
+  }
   console.log("getIcon", prefix, subspecifier, hexColor);
   const icon = await recolor(await handler(subspecifier), hexColor);
+  cache.set(cacheKey, icon);
   ctx.body = icon.data;
   ctx.set("Content-Type", icon.contentType);
 });
