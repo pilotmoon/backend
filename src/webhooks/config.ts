9117e592567fa22dffd3df67fda01a28a30463d6
Nick Moore <nick@pilotmoon.com>
2023-08-07 12:45:09 +0100

    It's working, at least

--- src/webhooks/config.ts
+++ src/webhooks/config.ts
@@ -10,7 +10,6 @@ export interface Config {
   ROLO_APIKEY_TEST: string;
   ROLO_APIKEY_LIVE: string;
   TWIX_APIKEYS: string;
-  SPACES_CONFIG: string;
 }
 export const config = loadConfig<Config>([
   { key: "TWIX_PORT", transform: decimalIntegerTransform },
@@ -25,5 +24,4 @@ export const config = loadConfig<Config>([
   { key: "ROLO_APIKEY_TEST", hidden: true },
   { key: "ROLO_APIKEY_LIVE", hidden: true },
   { key: "TWIX_APIKEYS", hidden: true },
-  { key: "SPACES_CONFIG", hidden: true },
 ]);
