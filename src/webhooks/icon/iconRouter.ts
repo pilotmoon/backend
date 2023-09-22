ebe9727392f691c1dc0fdd859b3f9de8fd166301
Nick Moore <nick@pilotmoon.com>
2023-09-22 02:10:19 +0100

    intrinsicPreserveColor takes precedence when true

--- src/webhooks/icon/iconRouter.ts
+++ src/webhooks/icon/iconRouter.ts
@@ -11,12 +11,12 @@ import { kebabCase } from "case-anything";
 export const router = new Router();
 
 function meta(icon: Icon, descriptor: IconDescriptor) {
-  const meta: Record<string, string> = {};
-  if (icon.preserveColor) {
-    meta["icon-preserve-color"] = "true";
-  }  
+  const meta: Record<string, string> = {};  
   for (const [k, v] of Object.entries(descriptor)) {
     meta[`icon-${kebabCase(k)}`] = encodeURI(String(v));
+  }
+  if (icon.intrinsicPreserveColor) {
+    meta["icon-preserve-color"] = "true";
   }
   console.log(meta);
   return meta;
