f117ef8135f29af9544d03f2016012232f788584
Nick Moore <nick@pilotmoon.com>
2023-11-21 15:45:36 +0000

    fix logger lint

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,14 @@
 // stubbed out for now
 import "colors";
+// biome-ignore lint/suspicious/noExplicitAny: mapping to underlying call
 export function log(...args: any[]) {
   console.log(...args);
 }
+// biome-ignore lint/suspicious/noExplicitAny: mapping to underlying call
 export function loge(...args: any[]) {
-  console.log("ERROR".bgRed, ...args);
+  console.log("ERROR".white.bgRed, ...args);
 }
+// biome-ignore lint/suspicious/noExplicitAny: mapping to underlying call
 export function logw(...args: any[]) {
   console.log("WARNING".yellow, ...args);
 }
