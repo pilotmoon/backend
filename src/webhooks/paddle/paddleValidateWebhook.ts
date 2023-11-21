c16a8a9265ed176a2334563790a45d113f82052d
Nick Moore <nick@pilotmoon.com>
2023-11-21 15:13:58 +0000

    fix linter error in paddleValidateWebhook

--- src/webhooks/paddle/paddleValidateWebhook.ts
+++ src/webhooks/paddle/paddleValidateWebhook.ts
@@ -1,7 +1,6 @@
 import { serialize } from "php-serialize";
 import { createVerify } from "node:crypto";
 import { z } from "zod";
-import _ from "lodash";
 import { Context } from "koa";
 
 const ZPaddleWebhookData = z
@@ -9,26 +8,24 @@ const ZPaddleWebhookData = z
     p_signature: z.string(),
   })
   .passthrough();
+type PaddleWebhookData = z.infer<typeof ZPaddleWebhookData>;
 
 // validate webhook signature (based on paddle docs)
 export function validateWebhook(ctx: Context, pubKey: string): boolean {
   try {
     const data = ZPaddleWebhookData.parse(ctx.request.body);
     // sort args by key and replace non-string values with stringified values
-    const sorted = {} as any;
-    for (const [key, val] of Object.entries(
-      _.omit(data, "p_signature"),
-    ).sort()) {
+    const sorted = {} as Record<string, unknown>;
+    for (const [key, val] of Object.entries(data).sort()) {
+      if (key === "p_signature") continue; // skip the signature
       if (typeof val === "string") {
         sorted[key] = val;
+      } else if (Array.isArray(val)) {
+        // is it an array
+        sorted[key] = val.toString();
       } else {
-        if (Array.isArray(val)) {
-          // is it an array
-          sorted[key] = val.toString();
-        } else {
-          //if its not an array and not a string, then it is a JSON obj
-          sorted[key] = JSON.stringify(val);
-        }
+        //if its not an array and not a string, then it is a JSON obj
+        sorted[key] = JSON.stringify(val);
       }
     }
     // verify the serialized array against the signature using SHA1 with the public key
