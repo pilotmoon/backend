7554eaa614b62edf0c2c7ed3449df8d0ec05514a
Nick Moore <nick@pilotmoon.com>
2023-11-23 14:07:18 +0000

    don't trim identifier when validating

--- src/common/saneSchemas.ts
+++ src/common/saneSchemas.ts
@@ -7,6 +7,5 @@ export const ZSaneQuantity = z.number().int().positive();
 export const ZSaneDate = z.coerce.date().min(new Date("2010-01-01"));
 export const ZSaneIdentifier = z
   .string()
-  .trim()
   .regex(/^[0-9a-zA-Z-_.]+$/)
   .max(100);
