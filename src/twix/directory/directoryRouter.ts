dad4eee2ef7a4bfbe85d4c08eb818cb25bf841f4
Nick Moore <nick@pilotmoon.com>
2023-12-14 12:12:05 +0000

    directory router faffing

--- src/twix/directory/directoryRouter.ts
+++ src/twix/directory/directoryRouter.ts
@@ -1,7 +1,7 @@
 import Router from "@koa/router";
 import { z } from "zod";
 import { log } from "../../common/log.js";
-import { validateGithubWebhook } from "../github.js";
+import { restClient as gh, validateGithubWebhook } from "../github.js";
 
 export const router = new Router();
 
@@ -13,12 +13,21 @@ const ZGithubTag = z.object({
   repository: z.object({
     html_url: z.string(),
     id: z.number(),
+    node_id: z.string(),
     name: z.string(),
+    private: z.boolean(),
+    full_name: z.string(),
     owner: z.object({
       login: z.string(),
       id: z.number(),
+      node_id: z.string(),
+      type: z.enum(["User", "Organization"]),
     }),
   }),
+});
+
+const ZWebhookParams = z.object({
+  noemail: z.string().optional(),
 });
 
 const GH_HOOK_PATH = "/webhooks/gh";
@@ -27,7 +36,38 @@ router
   .post(GH_HOOK_PATH, async (ctx) => {
     // console.log("GOT A WEBHOOK!", ctx.request.body);
     const tagInfo = ZGithubTag.parse(ctx.request.body);
+    log("query", ctx.request.query);
+    const params = ZWebhookParams.parse(ctx.request.query);
     log("GOT A TAG!", ctx.request.body);
-    log("tagInfo", tagInfo);
-    ctx.status = 200;
+    ctx.body = await processTag(tagInfo, params);
+    ctx.status = 202;
   });
+
+async function processTag(
+  tagInfo: z.infer<typeof ZGithubTag>,
+  params: z.infer<typeof ZWebhookParams>,
+) {
+  log("processTag", tagInfo);
+  const messages = [
+    `.-=beep boop=-. :: PopClip Extensions Directory :: Webhook Receiver :: ${new Date().toISOString()}`,
+    `GitHub webhook received from repo: ${tagInfo.repository.name}/${tagInfo.repository.owner.login}`,
+    `- New tag: ${tagInfo.ref}`,
+    `- Webhook URL params: ${params}`,
+  ];
+  if (tagInfo.repository.private) {
+    messages.push("This is a private repo -- no action taken");
+  }
+
+  const { data } = await gh().get(
+    `/repos/${tagInfo.repository.full_name}/git/trees/${tagInfo.ref}`,
+    { params: { recursive: 1 } },
+  );
+  log("tree", data);
+
+  const { data: datac } = await gh().get(
+    `/repos/${tagInfo.repository.full_name}/contents/`,
+  );
+  log("contents", datac);
+
+  return messages.join("\n");
+}
