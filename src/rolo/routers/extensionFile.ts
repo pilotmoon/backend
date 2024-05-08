9a68fbbf45d602952e3079a700bcbd2f4a81cb1b
Nick Moore <nick@pilotmoon.com>
2024-05-08 18:41:04 +0100

    add full v2 signature

--- src/rolo/routers/extensionFile.ts
+++ src/rolo/routers/extensionFile.ts
@@ -36,28 +36,36 @@ export async function generateExtensionFile(
     throw new ApiError(404, "Extension is not published");
   }
 
+  // flesh out file data
+  let files = ext.files.map((file) => ({
+    ...file,
+    data: file.data!,
+    executable: !!file.executable,
+  }));
+
   // add files to zip
   let packageName = `@${ext.shortcode}.${ext.info.identifier}.popclipext`;
   let zip = new AdmZip();
-  for (let file of ext.files) {
+  for (let file of files) {
     // note, files list has been pre-filtered in the aggregation
     zip.addFile(
       `${packageName}/${file.path}`,
-      file.data!,
+      file.data,
       undefined,
       file.executable ? 0o755 : 0o644,
     );
   }
 
   // add signature file
-  let key = await getExtensionSigningKey(authKind);
-  let signature = new Signer(key).extensionSignature(
-    ext.files.map((file) => ({
-      path: file.path,
-      data: file.data!,
-      executable: !!file.executable,
-    })),
+  let { key_v1, key_v2 } = await getExtensionSigningKeys(authKind);
+  let signature = await new Signer(key_v1, key_v2).extensionSignature(
+    files,
     packageName,
+    {
+      identifier: ext.info.identifier,
+      shortcode: ext.shortcode,
+      version: ext.version,
+    },
   );
   zip.addFile(`${packageName}/${signature.name}`, signature.contentsBuffer);
 
@@ -68,28 +76,35 @@ export async function generateExtensionFile(
   return { data: zip.toBuffer(), name };
 }
 
-const keyMap = new Map<AuthKind, string>();
-async function getExtensionSigningKey(authKind: AuthKind) {
-  let key = keyMap.get(authKind);
-  if (key) return key;
+const keyMap = new Map<AuthKind, { key_v1: string; key_v2: string }>();
+async function getExtensionSigningKeys(authKind: AuthKind) {
+  let keys = keyMap.get(authKind);
+  if (keys) return keys;
 
   const keysRecord = await getRegistryObjectInternal(
     "com.pilotmoon.popclip",
-    "extensionSigningKey",
+    "extensionSigningKeys",
     authKind,
   );
   if (!keysRecord) {
     throw new Error(`No popclipext signing key found for kind ${authKind}`);
   }
 
-  key = z
+  let keyRecord = z
     .object({
       object: z.literal("record"),
-      record: z.object({ privateKey: z.string() }),
+      record: z.object({
+        privateKey_v1: z.string(),
+        privateKey_v2: z.string(),
+      }),
     })
-    .parse(keysRecord).record.privateKey;
+    .parse(keysRecord);
 
   log("Loaded extension signing key", { authKind });
-  keyMap.set(authKind, key);
-  return key;
+  keys = {
+    key_v1: keyRecord.record.privateKey_v1,
+    key_v2: keyRecord.record.privateKey_v2,
+  };
+  keyMap.set(authKind, keys);
+  return keys;
 }
--- src/rolo/signext.ts
+++ src/rolo/signext.ts
@@ -3,37 +3,73 @@ import { makePlist } from "./makePlist.js";
 import { ZExtensionFileListEntry } from "../common/extensionSchemas.js";
 import { z } from "zod";
 import path from "node:path";
+import * as ed from "@noble/ed25519";
+import { log } from "../common/log.js";
 
-// utility for signing popclip extensions
+/*
+Utility for signing PopCLip extension packages
+*/
 
-export const ZDataFileListEntry = ZExtensionFileListEntry.omit({
-  hash: true,
-  size: true,
-}).required();
+// polyfill needed for @noble/ed25519
+import { webcrypto } from "node:crypto";
+// @ts-ignore
+if (!globalThis.crypto) globalThis.crypto = webcrypto;
+
+export const ZDataFileListEntry = ZExtensionFileListEntry.required();
 export type DataFileListEntry = z.infer<typeof ZDataFileListEntry>;
 
 export type KeyPair = { publicKey: bigint; privateKey: bigint };
 
 export class Signer {
-  private privateKey: crypto.KeyObject;
+  private privateKey_v1: crypto.KeyObject; // PEM RSA
+  private privateKey_v2: Buffer; // ed25519
 
-  // Create a new AquaticPrime instance with a public and private key
-  constructor(privateKeyPem: string) {
-    this.privateKey = crypto.createPrivateKey(privateKeyPem);
+  // create a new signer with private keys
+  constructor(privateKey_v1: string, privateKey_v2: string) {
+    this.privateKey_v1 = crypto.createPrivateKey(privateKey_v1.trim());
+    this.privateKey_v2 = Buffer.from(privateKey_v2, "base64");
   }
 
-  extensionSignature(files: DataFileListEntry[], packageName: string) {
-    const Signature = signData(dataToSign(files, packageName), this.privateKey);
-    const contentsBuffer = Buffer.from(makePlist({ Signature }));
+  async extensionSignature(
+    files: DataFileListEntry[],
+    packageName: string,
+    meta: Record<string, string>,
+  ) {
+    log("signing", { meta });
+    const Signature = signData_v1(
+      dataToSign(files, { mode: "v1", packageName }),
+      this.privateKey_v1,
+    );
+    const SignatureV2 = await signData_v2(
+      dataToSign(files, { mode: "v2", meta }),
+      this.privateKey_v2,
+    );
+    const contentsBuffer = Buffer.from(
+      makePlist({ Signature, SignatureV2, Metadata: meta }),
+    );
     return { name: "_Signature.plist", contentsBuffer };
   }
 }
 
-function signData(data: Buffer, key: crypto.KeyObject) {
+function signData_v1(data: Buffer, key: crypto.KeyObject) {
   return crypto.createSign("RSA-SHA1").update(data).sign(key);
 }
 
-function dataToSign(files: DataFileListEntry[], packageName: string) {
+async function signData_v2(data: Buffer, key: Buffer) {
+  return Buffer.from(await ed.signAsync(data, key));
+}
+
+export type DigestOptions =
+  | {
+      mode: "v1";
+      packageName: string;
+    }
+  | {
+      mode: "v2";
+      meta: Record<string, string>;
+    };
+
+function dataToSign(files: DataFileListEntry[], opts: DigestOptions) {
   // Sort array with case-insensitive comparison
   const sortedFiles = files.sort((a, b) =>
     a.path.localeCompare(b.path, "en-US", {
@@ -42,15 +78,39 @@ function dataToSign(files: DataFileListEntry[], packageName: string) {
     }),
   );
 
-  // Add each file to the data list
-  const separator = Buffer.from("+++");
-  const dataList = [Buffer.from(packageName)];
-  for (const file of sortedFiles) {
-    const baseName = path.basename(file.path);
-    dataList.push(separator);
-    dataList.push(Buffer.from(baseName));
-    dataList.push(separator);
-    dataList.push(file.data);
+  // V1 has some deficiences in the signature format.
+  // - Only the file's basename is included in the signature, so if it's in a
+  // subdirectory, the path can be changed without invalidating the signature.
+  // - The signature does not account for the file's executable bit.
+  // V2 fixes these issues by including the full path and the executable bit.
+  // V1 encodes the package name in the signature, while V2 does not.
+  // V2 add metadata to the signature.
+
+  const dataList: Buffer[] = [];
+  if (opts.mode === "v1") {
+    dataList.push(Buffer.from(opts.packageName));
+    for (const file of sortedFiles) {
+      dataList.push(Buffer.from(`+++${path.basename(file.path)}+++`));
+      dataList.push(file.data);
+    }
+  } else {
+    const dataList = [Buffer.from(`popclipext ${sortedFiles.length}\0`)];
+    // identifier ${opts.identifier}\0shortcode ${opts.shortcode}\0version ${opts.version}\0\0
+    const metaString = Object.entries(opts.meta)
+      .sort(([keyA], [keyB]) => keyA.localeCompare(keyB))
+      .map(([key, value]) => `${key} ${value}\0`)
+      .join("");
+    log({ metaString });
+    dataList.push(Buffer.from(`${metaString}\0`));
+    for (const file of sortedFiles) {
+      dataList.push(
+        Buffer.from(
+          `${file.hash} ${file.executable ? "1" : "0"} ${file.size} ${
+            file.path
+          }\0}`,
+        ),
+      );
+    }
   }
   return Buffer.concat(dataList);
 }
