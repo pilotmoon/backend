2b487ca4f92e1eb07921667cbde534d7d1641e2f
Nick Moore <nick@pilotmoon.com>
2024-05-13 11:49:42 +0100

    fix display url in remote log

--- src/twix/middleware/openRemoteLog.ts
+++ src/twix/middleware/openRemoteLog.ts
@@ -2,9 +2,8 @@ import Koa from "koa";
 import { ActivityLog } from "../activityLog.js";
 import { config } from "../config.js";
 
-const ROLO_URL = config.ROLO_URL;
 const ROLO_AUTH_KIND = "live";
-// const ROLO_AUTH_KIND = "test";
+const ROLO_DISPLAY_URL = config.ROLO_URL_CANONICAL;
 
 // middleware to open a remote log and make it available to the context
 export async function openRemoteLog(ctx: Koa.Context, next: Koa.Next) {
@@ -13,7 +12,9 @@ export async function openRemoteLog(ctx: Koa.Context, next: Koa.Next) {
     `${ctx.request.method} ${ctx.request.url}`,
   );
   if (url) {
-    ctx.alog.log(`-=-\nRemote log:\n${ROLO_URL}${url}&format=text\n-=-`);
+    ctx.alog.log(
+      `-=-\nRemote log:\n${ROLO_DISPLAY_URL}${url}&format=text\n-=-`,
+    );
   } else {
     ctx.alog.log("Failed to create remote log");
   }
